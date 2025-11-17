d79ea7541c05e5660571b9f0fc5ca9669fbac8e7
Tuananh <tuananh48218@gmail.com>
2025-11-17 19:59:39 +0700

    feat: integrate Swagger for API documentation and update dependencies

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,21 +8,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AllExceptionFilter } from './common/filters/exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
-  
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new AllExceptionFilter());
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('Auction-Hub API')
+    .setDescription('The Auction API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, documentFactory);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
